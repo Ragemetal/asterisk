d84aa2aa88f8f532b460062858d36b6d3ab68cce
Ragemetal <107939896+Ragemetal@users.noreply.github.com>
2022-07-27 10:27:23 +0300

    Update file.py

--- file.py
+++ file.py
@@ -22,12 +22,3 @@ with open('output_file.txt', 'w') as fo:
     for number in phone_list:
         fo.write('\t_%s => {\n\t\tNoOp(Permission denied);\n\t\tHangup();\n\t};\n' % number)
     fo.write(end_string)
-
-
-start_string = 'context atr-operators {\n'
-end_string = '};'
-
-
-
-
-
